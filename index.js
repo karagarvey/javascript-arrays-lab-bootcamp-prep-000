5f372e9561db69a001f8d4aec56c8bfa31338be4
karagarvey <kgarvey87@gmail.com>
2018-03-13 18:52:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  [...kittens, name]
+  return kittens
+}
+
+function prependKitten(name) {
+  [name, ...kittens]
+  return kittens
+}
+
+function removeLastKitten() {
+  kittens[kittens.length-1]
+}
